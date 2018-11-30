513df300e082f68fdb47a17eec80bbe3ca282eae
shz <shz@qq.com>
2018-11-30 15:41:16 +0800

    框架搭建

---
+++ BuildingBlocks/SmartCqrs.Infrastructure/Log/ILoggerManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartCqrs.Infrastructure.Log
+{
+    public interface ILoggerManager
+    {
+        void Debug(string message);
+        void Info(string message);
+        void Warn(string message, Exception ex = null);
+        void Error(string message, Exception ex = null);
+    }
+}
---
+++ BuildingBlocks/SmartCqrs.Infrastructure/Log/NLoggerManager.cs
@@ -0,0 +1,44 @@
+﻿using NLog;
+using System;
+
+namespace SmartCqrs.Infrastructure.Log
+{
+    public class NLoggerManager : ILoggerManager
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public void Debug(string message)
+        {
+            logger.Debug(message);
+        }
+
+        public void Error(string message, Exception ex = null)
+        {
+            if (ex == null)
+            {
+                logger.Error(message);
+            }
+            else
+            {
+                logger.Error(ex, message);
+            }
+        }
+
+        public void Info(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void Warn(string message, Exception ex = null)
+        {
+            if (ex == null)
+            {
+                logger.Warn(message);
+            }
+            else
+            {
+                logger.Warn(ex, message);
+            }
+        }
+    }
+}
