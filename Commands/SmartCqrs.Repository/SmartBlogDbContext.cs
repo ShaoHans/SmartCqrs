eeb63ffb1ebf29689f707a8c77be52948879cc51
shz <shz@qq.com>
2018-12-05 11:51:34 +0800

    update

--- Commands/SmartCqrs.Repository/SmartBlogDbContext.cs
+++ Commands/SmartCqrs.Repository/SmartBlogDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DocsByReflection;
+using Microsoft.EntityFrameworkCore;
 using SmartCqrs.Domain.Models;
 using System;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
+using System.Xml;
 
 namespace SmartCqrs.Repository
 {
@@ -57,21 +59,32 @@ namespace SmartCqrs.Repository
         {
             // 参考文章
             // https://andrewlock.net/customising-asp-net-core-identity-ef-core-naming-conventions-for-postgresql/
-
             foreach (var entity in modelBuilder.Model.GetEntityTypes())
             {
+
                 //Replace table names
                 entity.Relational().TableName = entity.Relational().TableName.ToSnakeCase();
 
                 // Replace column names            
                 foreach (var property in entity.GetProperties())
                 {
-                    // 当类属性加上Description标注，可以当作数据库字段说明
-                    var desAttr = property.PropertyInfo.GetCustomAttribute<DescriptionAttribute>();
-                    if (desAttr != null)
-                    {
-                        property.Npgsql().Comment = desAttr.Description;
-                    }
+                    // 生成数据库字段注释的两种方式
+                    // 1.给类属性加上Description标注，可以当作数据库字段说明
+                    //var desAttr = property.PropertyInfo.GetCustomAttribute<DescriptionAttribute>();
+                    //if (desAttr != null)
+                    //{
+                    //    property.Npgsql().Comment = desAttr.Description;
+                    //}
+
+                    // 2.通过反射的方式获取类属性的注解
+                    //var propertyInfo = entity.ClrType.GetProperty(property.Name);
+                    //XmlElement documentation = DocsService.GetXmlFromMember(propertyInfo);
+                    //if (documentation != null && !string.IsNullOrWhiteSpace(documentation.InnerText))
+                    //{
+                    //    property.Npgsql().Comment = documentation.InnerText;
+                    //}
+
+                    // 字段名重命名
                     property.Relational().ColumnName = property.Name.ToSnakeCase();
                 }
 
